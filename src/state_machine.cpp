2e1f5405ea95619c9d2a37ba951a64817a6a8e7b
Madhav Kanbur <abcdjdj@gmail.com>
2022-03-31 08:59:56 -0600

    Better fix instead of sleep(2)
    * Don't use locks for case when both are in STATE_START!
    
    Signed-off-by: Madhav Kanbur <abcdjdj@gmail.com>

--- src/state_machine.cpp
+++ src/state_machine.cpp
@@ -19,22 +19,22 @@ void StateMachine::setState(enum ServerStates target_state)
 void StateMachine::initState(PrimaryBackupRPCClient *g_RPCCLient)
 {
     int state_other, ret;
+    if(state == STATE_START) {
+        state_other = g_RPCCLient->GetState(5);
+        if(state_other == STATE_START) {
+             setState(DEFAULT_ROLE); // Fed in from CMakeLists.txt as a -D compiler constant
+             return;
+        }
+    }
+
     switch(state) {
         case STATE_START:
-        if(DEFAULT_ROLE == STATE_PRIMARY)
-            sleep(2);
         pthread_mutex_lock(&BACKUP_TRANSITION_LOCK);
         state_other = g_RPCCLient->GetState(5);
 
         if (state_other == -1) {
             setState(STATE_PRIMARY); // If no response from other server, become the PRIMARY
             pthread_mutex_unlock(&BACKUP_TRANSITION_LOCK);
-        } else if (state_other == STATE_START) {
-            // Race Condition here! What if both servers have acquired their
-            // BACKUP_TRANSITION_LOCKs and now both call GetState() RPCs. HANG!
-            // That's why add a sleep(2) in case STATE_START to break the contention.
-            pthread_mutex_unlock(&BACKUP_TRANSITION_LOCK);
-            setState(DEFAULT_ROLE); // Fed in from CMakeLists.txt as a -D compiler constant
         } else if (state_other == STATE_PRIMARY) {
             pthread_mutex_unlock(&BACKUP_TRANSITION_LOCK);
             // We become the backup after RESYNCING
@@ -44,6 +44,9 @@ void StateMachine::initState(PrimaryBackupRPCClient *g_RPCCLient)
         } else if (state_other == STATE_BACKUP) {
             // Become the primary; ensure we don't have 2 PRIMARIES
             setState(STATE_PRIMARY);
+            pthread_mutex_unlock(&BACKUP_TRANSITION_LOCK);
+        } else {
+            std::cout << "Other state unknown = " << state_other << "\n";
             pthread_mutex_unlock(&BACKUP_TRANSITION_LOCK);
         }
         break;
