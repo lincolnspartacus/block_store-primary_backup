a3ac0adec0176b66e3a7c6485551b51987cc71d3
pandotra <pandotra@node-2.pandotra-121887.advosuwmadison-pg0.wisc.cloudlab.us>
2022-04-02 18:02:35 -0500

    Random writes test case
    Signed-off-by: pandotra <pandotra@node-2.pandotra-121887.advosuwmadison-pg0.wisc.cloudlab.us>

--- src/client.cpp
+++ src/client.cpp
@@ -6,6 +6,50 @@
 
 RPCClientLibrary *gRPCClient = nullptr;
 
+int compare_buf(uint8_t *buf1, uint8_t *buf2)
+{
+    for(int i = 0; i < 4096; ++i)
+      if(buf1[i] != buf2[i])
+        return 1;
+
+    return 0;
+}
+
+void write_random_non_overlapping()
+{
+    constexpr int blocks = 4;
+    int offsets[blocks] = {0, 4096, 4096*2 + 234, 4096*10 + 76};
+    uint8_t *buf[blocks];
+
+    for(int i = 0; i < blocks; ++i)
+      buf[i] = new uint8_t[4096];
+
+    for(int i = 0; i < blocks; ++i) {
+      // Fill up buf with random data
+      for(int j = 0; j < 4096; ++j)
+        buf[i][j] = (uint8_t)rand();
+      
+      int answer = gRPCClient->WriteBlock(offsets[i], buf[i]);
+      printf("Write Ret for Block %d = %d\n", i, answer);
+    }
+
+    char user_input[10];
+    printf("Go crash the server now!\n");
+    scanf("%s", user_input);
+
+    // Read back the same offsets and compare it
+    uint8_t rcv_buf[4096];
+    for(int i = 0; i < blocks; ++i) {
+      int answer = gRPCClient->ReadBlock(offsets[i], rcv_buf);
+      if(compare_buf(rcv_buf, buf[i]) == 1) {
+          printf("write_random_non_overlapping() : Test failed!\n");
+          return;
+      }
+    }
+
+    printf("write_random_non_overlapping() : Test passed!\n");
+}
+
 int main(int argc, char *argv[])
 {
 
@@ -14,24 +58,19 @@ int main(int argc, char *argv[])
   serverString.push_back("c220g1-031114.wisc.cloudlab.us:50051"); // Node 0
   serverString.push_back("c220g1-031111.wisc.cloudlab.us:50051"); // Node 1
 
-  RPCClientLibrary *gRPCClient = new RPCClientLibrary(serverString);
+  gRPCClient = new RPCClientLibrary(serverString);
 
-  // int sent = 10;
-  // int answer = gRPCClient->DoMessageInt(sent);
-
-  // printf("Sent is %d\n", sent);
-  // printf("Reply is %d\n", answer);
-
-  printf("Testing read & write: \n");
   uint8_t *buf = new uint8_t[4096];
-  // answer = gRPCClient->ReadBlock(56, buf);
-  // printf("Read Ret = %d\n", answer);
-  // for(int i = 0; i < 4096; ++i)
-  //   printf("%x ", buf[i]);
 
-  memset(buf, 66, 4096);
-  int answer = gRPCClient->WriteBlock(0, buf);
-  printf("Write Ret = %d\n", answer);
+  // while(1){
+  //   memset(buf, 66, 4096);
+  //   int answer = gRPCClient->WriteBlock(0, buf);
+  //   printf("Write Ret = %d\n", answer);
+
+  //   char user_input[10];
+  //   scanf("%s", user_input);
+  // }
+  write_random_non_overlapping();
 
   return 0;
 }
